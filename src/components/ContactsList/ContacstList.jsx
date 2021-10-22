c0817f487b57b4e253f607c8cff7b6552c274914
Andrew Yatsutko <eitherwiser@gmail.com>
2021-10-23 02:30:00 +0300

    finish

---
+++ src/components/ContactsList/ContacstList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./ContactsList.module.css";
+
+export default function ContacstList({ contacts, onDelete }) {
+  return (
+    <ul className={s.list}>
+      <h6>Contacts list</h6>
+        {contacts.map(({id, name, number}) => (
+          <li key={id} id={id} className={s.list_item}>
+            <span>{name}&nbsp;:</span>
+            <span>{number}</span>
+        <button type="button" className={s.btn}
+          onClick={(e) => onDelete(e.currentTarget.parentNode.id)}
+          >
+          <span class="material-icons">
+delete
+</span>
+        </button>
+      </li>
+        ))}
+      </ul>
+  );
+}
+
+ContacstList.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+
+//onDelete(e.currentTarget.parentNode.key
